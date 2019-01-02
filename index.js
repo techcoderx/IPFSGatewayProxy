3537633c552f3c083ff1864dbf0042ca8b1ece92
techcoderx <engtianxi@outlook.com>
2019-01-02 14:24:35 +0800

    regex & writehead 404

--- index.js
+++ index.js
@@ -5,26 +5,28 @@ const app = Express();
 const ProxyAPI = Proxy.createProxyServer();
 const Config = {
     IPFSGateway: 'http://localhost:8080',
-    Redirect404: 'https://uploader.oneloved.tube/404',
     Port: 3000
 }
 
 app.all('/ipfs/*', (req,res) => {
     let hash = req.originalUrl.split('/')[2];
+    if (/^[a-zA-Z0-9]*$/.test(hash) == false || hash.length != 46) {
+        res.writeHead(404);
+        return res.end();
+    }
     Shell.exec('ipfs pin ls -t recursive',{silent: true},(code,stdout,stderr) => {
-        if (stderr != '')
+        if (stderr != '') 
             console.log('\n\nError: ' + stderr);
         
         if (stdout.includes(hash)) {
             ProxyAPI.web(req,res,{target: Config.IPFSGateway});
         } else {
-            res.status(404).redirect(Config.Redirect404);
+            res.writeHead(404);
+            res.end();
         }
     })
 });
 
-ProxyAPI.on('error',(err) => {
-    console.log(err);
-});
+ProxyAPI.on('error',(err) => console.log(error));
 
 app.listen(Config.Port);
